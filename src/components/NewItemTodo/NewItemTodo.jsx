f9a2a8f0933d76604bc8055a11928229c18c0705
User <golova92-69@yandex.ru>
2019-06-10 13:50:52 +0300

    Change component NewItemTodo to pure component

--- src/components/NewItemTodo/NewItemTodo.jsx
+++ src/components/NewItemTodo/NewItemTodo.jsx
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './NewItemTodo.css';
 import PropTypes from 'prop-types';
 
-export default class NewItemTodo extends Component {
-  handleBlur = (e) => {
-    this.createElement(e);
-  };
-
-  handleKeyDown = (e) => {
-    this.createElement(e);
-  };
-
-  createElement(e) {
+const NewItemTodo = ({
+  onAdd,
+}) => {
+  const createElement = (e) => {
     if (!e.keyCode || e.keyCode === 13) {
       if (e.target.value !== '') {
-        const { onAdd } = this.props;
         const obj = { id: Date.now(), text: e.target.value, isComplete: false };
         onAdd(obj);
         e.target.value = '';
       }
     }
-  }
+  };
 
-  render() {
-    return (
-      <input
-        className="header__new-item"
-        onBlur={this.handleBlur}
-        onKeyDown={this.handleKeyDown}
-        placeholder="What needs to be done?"
-      />
-    );
-  }
-}
+  const handleBlur = (e) => {
+    createElement(e);
+  };
+
+  const handleKeyDown = (e) => {
+    createElement(e);
+  };
+
+
+  return (
+    <input
+      className="header__new-item"
+      onBlur={handleBlur}
+      onKeyDown={handleKeyDown}
+      placeholder="What needs to be done?"
+    />
+  );
+};
 
 NewItemTodo.propTypes = {
   onAdd: PropTypes.func.isRequired,
 };
+export default NewItemTodo;
